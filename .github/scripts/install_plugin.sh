74700c42ae5a2cf138a7633fcb93699dc88081a7
Roman Plotnikov <rplotnikov@k2.cloud>
2025-05-28 19:47:38 +0300

    improve install script with text output

--- .github/scripts/install_plugin.sh
+++ .github/scripts/install_plugin.sh
@@ -2,3 +2,4 @@
 
 cp -f /home/plorov/Study/ASR_OBS_Plugin/release/RelWithDebInfo/lib/x86_64-linux-gnu/obs-plugins/asr_obs_plugin.so /usr/lib/x86_64-linux-gnu/obs-plugins/.
 cp -rf /home/plorov/Study/ASR_OBS_Plugin/release/RelWithDebInfo/share/obs/obs-plugins/asr_obs_plugin/locale /usr/share/obs/obs-plugins/asr_obs_plugin/.
+echo "Plugin 'asr_obs_plugin' installed"
