e6d556a1949a08177b383935d16bd244ee980c1f
Roman Plotnikov <rplotnikov@k2.cloud>
2025-05-31 01:59:29 +0300

    add option to run --with-deps

--- .github/scripts/install_plugin.sh
+++ .github/scripts/install_plugin.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-/home/plorov/Study/ASR_OBS_Plugin/.github/scripts/build-ubuntu --skip-deps
+BUILD_ARGS="--skip-deps"
+if [[ "$1" == "--with-deps" ]]; then
+    BUILD_ARGS=""
+fi
+/home/plorov/Study/ASR_OBS_Plugin/.github/scripts/build-ubuntu $BUILD_ARGS
+
 cp -f /home/plorov/Study/ASR_OBS_Plugin/release/RelWithDebInfo/lib/x86_64-linux-gnu/obs-plugins/asr_obs_plugin.so /usr/lib/x86_64-linux-gnu/obs-plugins/.
 cp -rf /home/plorov/Study/ASR_OBS_Plugin/release/RelWithDebInfo/share/obs/obs-plugins/asr_obs_plugin/locale /usr/share/obs/obs-plugins/asr_obs_plugin/.
 echo "Plugin 'asr_obs_plugin' installed"
